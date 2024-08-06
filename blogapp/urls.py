92e46d2c722893cd063218ebc1a1be52267fd621
Rana-Tek <ranatekbas122@gmail.com>
2024-08-06 23:06:27 +0300

    blog app urls path, include and django urls

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,3 +1,12 @@
 from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from . import views
+
+
 
 router = DefaultRouter()
+router.register("blogs", views.PostModelViewSet)
+
+urlpatterns = [
+path("", include(router.urls)),    
+]
