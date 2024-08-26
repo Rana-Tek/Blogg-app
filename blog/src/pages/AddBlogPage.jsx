5b57df15061bd56f18f621703f38549c4829383a
Rana-Tek <ranatekbas122@gmail.com>
2024-08-26 04:58:51 +0300

    useState for title and content and handleSubmit described for form.

--- blog/src/pages/AddBlogPage.jsx
+++ blog/src/pages/AddBlogPage.jsx
@@ -1,21 +1,39 @@
+import { useState } from "react"
+
 const AddBlogPage = () => {
+
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+  
+  const newBlog = {
+    title:title,
+    content:content
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(newBlog)
+  }
+
     return(
         <div className="flex justify-center items-center h-screen mt-10 border-purple-900">
-        <form className="w-full max-w-lg bg-white p-8 rounded-lg shadow-lg">
+        <form className="w-full max-w-lg bg-white p-8 rounded-lg shadow-lg" onSubmit={handleSubmit}>
           <p className="text-2xl mb-4 text-center font-semibold">Add a New Blog</p>
           <div className="mb-8">
             <label htmlFor="input" className="block text-gray-700 text-sm font-bold mb-2">Title</label>
             <input 
               id="input"
+              onChange={(e) => setTitle(e.target.value)}
               className="border-2 border-purple-900 rounded w-full py-2 px-3 text-gray-950 leading-tight focus:outline-none focus:shadow-outline" 
               placeholder="Enter your input"
               required 
             />
           </div>
           <div className="mb-8">
-            <label htmlFor="textarea" className="block text-gray-700 text-sm font-bold mb-2">Body</label>
+            <label htmlFor="textarea" className="block text-gray-700 text-sm font-bold mb-2">Content</label>
             <textarea 
               id="textarea"  
+              onChange={(e) => setContent(e.target.value)}
               className="border-2 border-purple-900 rounded w-full py-2 px-3 text-gray-950 leading-tight focus:outline-none focus:shadow-outline" 
               rows="6" 
               placeholder="Enter your message" 
