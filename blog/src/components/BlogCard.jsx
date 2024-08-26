185c841401a25d3230784f530db9a521b0536b3b
Rana-Tek <ranatekbas122@gmail.com>
2024-08-26 04:14:38 +0300

    add dynamic data and substring method for content

--- blog/src/components/BlogCard.jsx
+++ blog/src/components/BlogCard.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom"
 const BlogCard = ({blog}) => {
+    let content = blog.content.substring(0, 250) + " ....."
+
     return(
         <div className="max-w-sm bg-white mb-6 border border-gray-200 rounded-lg shadow">
         <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
@@ -7,7 +9,7 @@ const BlogCard = ({blog}) => {
             <h5 className="mb-2 text-2xl font-bold tracking-tight">{blog?.title} </h5>
             
             <p className="mb-3 font-normal text-gray-700">
-            JavaScript, a dynamic programming language, primarily used for web development, enables interactive web pages. It's client-side, meaning it runs on us'
+           {content}
             </p>
             <Link to="/blogs/slug" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Read more
